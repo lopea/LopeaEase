fc8a55957a3f05013397e011cc23b4a48eba8eaa
Javier Sandoval <Javiers2001@gmail.com>
2019-10-16 08:33:15 -0700

    added code

--- Assets/Easing/Easing.cs
+++ Assets/Easing/Easing.cs
@@ -7,34 +7,35 @@ using UnityEngine; /* Mathf */
 
 namespace Lopea.Utils
 {
-    public static class Ease
+  public static class Ease
+  {
+    public static float cubicIn(float a, float b, float t)
     {
-        public static float cubicIn(float a, float b, float t)
-        {
-            return a + (b - a) * t * t * t;
-        }
-        public static float cubicOut(float a,float b,float t)
-        {
-            return a + (b - a) * (1 - Mathf.Pow(1-t, 3));
-        }
-        public static float cubicInOut(float a,float b,float t)
-        {
-            return (t > 0.5) ?
-                a + (b - a) * (4 * Mathf.Pow(t, 3)) :
-                a + (b - a) * (1 - Mathf.Pow(-2 * t + 2, 2) / 2);
-        }
-        public static float sinIn(float a,float b,float t)
-        {
-            return a + (b - a) * (1-Mathf.Cos(t * Mathf.PI/2));
-        }
-        public static float sinOut(float a,float b,float t)
-        {
-            return a + (b-a) * -(Mathf.Cos())
-        }
-        public static float sinInOut(float a,float b,float t)
-        {
-
-        }
-        
+      if(t > 1)
+        return b;
+      return a + (b - a) * t * t * t;
     }
+    public static float cubicOut(float a, float b, float t)
+    {
+      return a + (b - a) * (1 - Mathf.Pow(1-t, 3));
+    }
+    public static float cubicInOut(float a, float b, float t)
+    {
+      return (t > 0.5) ?
+        a + (b - a) * (4 * Mathf.Pow(t, 3)) :
+        a + (b - a) * (1 - Mathf.Pow(-2 * t + 2, 2) / 2);
+    }
+    public static float sinIn(float a, float b, float t)
+    {
+      return a + (b - a) * (1-Mathf.Cos(t * Mathf.PI/2));
+    }
+    public static float sinOut(float a, float b,float t)
+    {
+      return a + (b-a) * Mathf.Sin(t * Mathf.PI / 2);
+    }
+    public static float sinInOut(float a, float b, float t)
+    {
+      return a + (b - a) * -(Mathf.Cos(t * Mathf.PI) - 1) / 2;
+    }
+  }
 }
